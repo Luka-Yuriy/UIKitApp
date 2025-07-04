26a9108d7666951d608b55f7e49190363c9235d6
Yuri Luka <lukayurii.work@gmail.com>
2025-07-04 10:33:48 +0500

    Added segmented control

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
+    @IBOutlet var segmentedCotroller: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        segmentedCotroller.insertSegment(withTitle: "Trird", at: 2, animated: false)
+        setupMainLabel()
     }
-
+    
+    // MARK: - IB Actions
+    @IBAction func segmentedControllerAction() {
+        
+    }
+    
+    // MARK: - Private Methods
+    private func setupMainLabel () {
+        mainLabel.text = ""
+        mainLabel.font = UIFont.systemFont(ofSize: 35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+    }
 
 }
 
